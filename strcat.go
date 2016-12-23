835015703ba018942146488e65b03ca195db0e6e
Iavael <iavael@users.noreply.github.com>
2016-12-23 03:31:50 +0300

    Make golint happy

--- strcat.go
+++ strcat.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 )
 
-// Concat slice of strings in one string
 func strfmtBase(args []string) *bytes.Buffer {
 	result := bytes.NewBufferString("")
 	for _, arg := range args {
@@ -13,22 +12,26 @@ func strfmtBase(args []string) *bytes.Buffer {
 	return result
 }
 
+// StrCat concatenates string arguments in one string
 func StrCat(args ...string) string {
 	result := strfmtBase(args)
 	return result.String()
 }
 
+// StrCatln concatenates string arguments in one string with newline
 func StrCatln(args ...string) string {
 	result := strfmtBase(args)
 	result.WriteRune('\n')
 	return result.String()
 }
 
+// StrCatS concatenates slice of strings in one string
 func StrCatS(args []string) string {
 	result := strfmtBase(args)
 	return result.String()
 }
 
+// StrCatSln concatenates slice of strings in one string with newline
 func StrCatSln(args []string) string {
 	result := strfmtBase(args)
 	result.WriteRune('\n')
