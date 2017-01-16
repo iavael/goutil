de65ffa5c007b8230d012d49c8fdd01b80ae53bf
Iavael <iavael@users.noreply.github.com>
2017-01-16 23:25:40 +0300

    Add NetMultiConfig function to support multiple config version at the same time

--- config.go
+++ config.go
@@ -8,9 +8,14 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+// IConfigVer interface for checking version
+type IConfigVer interface {
+	GetVer() int
+}
+
 // IConfig interface for checking version and general config validation
 type IConfig interface {
-	GetVer() int
+	IConfigVer
 	Check() error
 }
 
@@ -33,4 +38,30 @@ func NewConfig(file string, c IConfig, vers []int) error {
 		return errors.New(msg)
 	}
 	return c.Check()
+}
+
+// NewMultiConfig for parsing and validating YAML config and map to config-dependent structs
+func NewMultiConfig(file string, cfgver IConfigVer, cfgmap func(int) IConfig) (IConfig, error) {
+	var (
+		buf []byte
+		err error
+		cfg IConfig
+	)
+	if buf, err = ioutil.ReadFile(file); err != nil {
+		msg := StrCat("Failed to open config: ", err.Error())
+		return nil, errors.New(msg)
+	}
+	if err = yaml.Unmarshal(buf, cfgver); err != nil {
+		msg := StrCat("Invalid config format: ", err.Error())
+		return nil, errors.New(msg)
+	}
+	cfg = cfgmap(cfgver.GetVer())
+	if cfg == nil {
+		msg := StrCat("Unsupported config version ", strconv.Itoa(cfgver.GetVer()))
+		return nil, errors.New(msg)
+	} else if err = yaml.Unmarshal(buf, cfg); err != nil {
+		msg := StrCat("Invalid config format: ", err.Error())
+		return nil, errors.New(msg)
+	}
+	return cfg, cfg.Check()
 }
